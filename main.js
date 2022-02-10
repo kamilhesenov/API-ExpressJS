185dad316b58187993ede6318784794143e3b260
kamilhasanov <kamilah@code.edu.az>
2022-02-10 17:50:52 +0400

    Маршрутизация

--- main.js
+++ main.js
@@ -4,10 +4,24 @@ import express from "express";
 const port = 8000;
 const app = express();
 
-app.get("/hello",(request,response)=>{
-    response.send("Hello Express");
+app.all("/hello", (request, response, next) => {
+    console.log("All");
+    next();
 })
 
-app.listen(port,()=>{
+const cb = (request, response, next) => {
+    console.log("CB");
+    next();
+}
+
+app.route("/user")
+    .get("/hello", cb, (request, response) => {
+        response.send("Hello Express");
+    })
+    .post("/hello", (request, response) => {
+        response.send("Hello Express POST");
+    })
+
+app.listen(port, () => {
     console.log(`Сервер запущен на http://localhost:${port}`);
 })
